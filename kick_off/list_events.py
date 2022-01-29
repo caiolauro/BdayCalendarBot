353812f4a5fe7059ae4106bb16550fc569d08f27
Caio Lauro <clauro@galileo-ft.com>
2022-01-29 08:51:46 -0300

    get 2022 events

--- kick_off/list_events.py
+++ kick_off/list_events.py
@@ -1,22 +1,23 @@
 import datetime
 from cal_setup import get_calendar_service
+event_list = []
+def get_events_list():
+    service = get_calendar_service()
+    # Call the Calendar API
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    print('Getting List of events')
+    events_result = service.events().list(
+        calendarId='primary', timeMin=now,
+        singleEvents=True,
+        orderBy='startTime',).execute()
+    events = events_result.get('items', [])
 
-def main():
-   service = get_calendar_service()
-   # Call the Calendar API
-   now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-   print('Getting List o 10 events')
-   events_result = service.events().list(
-       calendarId='primary', timeMin=now,
-       maxResults=10, singleEvents=True,
-       orderBy='startTime').execute()
-   events = events_result.get('items', [])
-
-   if not events:
-       print('No upcoming events found.')
-   for event in events:
-       start = event['start'].get('dateTime', event['start'].get('date'))
-       print(start, event['summary'])
-
-if __name__ == '__main__':
-   main()
+    if not events:
+        print('No upcoming events found.')
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        if start[0:4] == '2022':
+            event_list.append(dict(date = start, name =  event['summary']))
+    print(event_list)
+    return event_list
+get_events_list()
